19a5e806d95b60e4dd90fc73603e9d2900f64d5c
Adriana <ramiro.di.rico@hotmail.com>
2018-10-09 20:50:05 -0300

    created reminder and notifications to lets a user creates monthly reminders of things that will or have to pay or cash. it is a work in progress.

---
+++ HomeManagement.Mapper/NotificationMapper.cs
@@ -0,0 +1,31 @@
+﻿using HomeManagement.Contracts.Mapper;
+using HomeManagement.Domain;
+using HomeManagement.Models;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace HomeManagement.Mapper
+{
+    public interface INotificationMapper : IMapper<Notification, NotificationModel>
+    {
+
+    }
+
+    public class NotificationMapper : BaseMapper<Notification, NotificationModel>, INotificationMapper
+    {
+        public override IEnumerable<PropertyInfo> GetEntityProperties()
+        {
+            yield return typeof(Account).GetProperty(nameof(Notification.Id));
+            yield return typeof(Account).GetProperty(nameof(Notification.ReminderId));
+        }
+
+        public override IEnumerable<PropertyInfo> GetModelProperties()
+        {
+            yield return typeof(Account).GetProperty(nameof(NotificationModel.Id));
+            yield return typeof(Account).GetProperty(nameof(NotificationModel.ReminderId));
+            yield return typeof(Account).GetProperty(nameof(NotificationModel.Title));
+            yield return typeof(Account).GetProperty(nameof(NotificationModel.Description));
+            yield return typeof(Account).GetProperty(nameof(NotificationModel.DueDay));
+        }
+    }
+}
---
+++ HomeManagement.Mapper/ReminderMapper.cs
@@ -0,0 +1,33 @@
+﻿using HomeManagement.Contracts.Mapper;
+using HomeManagement.Domain;
+using HomeManagement.Models;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace HomeManagement.Mapper
+{
+    public interface IReminderMapper : IMapper<Reminder, ReminderModel>
+    {
+    }
+
+    public class ReminderMapper : BaseMapper<Reminder, ReminderModel>, IReminderMapper
+    {
+        public override IEnumerable<PropertyInfo> GetEntityProperties()
+        {
+            yield return typeof(Account).GetProperty(nameof(Reminder.Id));
+            yield return typeof(Account).GetProperty(nameof(Reminder.Title));
+            yield return typeof(Account).GetProperty(nameof(Reminder.Description));
+            yield return typeof(Account).GetProperty(nameof(Reminder.DueDay));
+            yield return typeof(Account).GetProperty(nameof(Reminder.Active));
+        }
+
+        public override IEnumerable<PropertyInfo> GetModelProperties()
+        {
+            yield return typeof(Account).GetProperty(nameof(ReminderModel.Id));
+            yield return typeof(Account).GetProperty(nameof(ReminderModel.Title));
+            yield return typeof(Account).GetProperty(nameof(ReminderModel.Description));
+            yield return typeof(Account).GetProperty(nameof(ReminderModel.DueDay));
+            yield return typeof(Account).GetProperty(nameof(ReminderModel.Active));
+        }
+    }
+}
