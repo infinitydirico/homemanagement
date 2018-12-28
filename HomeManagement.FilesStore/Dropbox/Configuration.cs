ff7c3e6493143ceb9a2ec64d9be6492413815b7b
Ramiro Di Rico <ramiro.dirico@projekt202.com>
2018-12-28 11:27:28 -0300

    missing untracked files, Store csproj

---
+++ HomeManagement.FilesStore/Dropbox/Configuration.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeManagement.FilesStore.Dropbox
+{
+    public class Configuration
+    {
+        public string AppId { get; set; }
+
+        public string AppSecret { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public bool IsInitialzed() => !string.IsNullOrEmpty(AppId) && !string.IsNullOrEmpty(AppSecret);
+    }
+}
