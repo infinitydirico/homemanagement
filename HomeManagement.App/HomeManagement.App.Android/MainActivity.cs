7854ed7000638bed0bdc222a342d5da3d486a88d
Ramiro <ramiro.di.rico@hotmail.com>
2019-06-13 08:44:35 -0300

    Added splashscreen for android.

--- HomeManagement.App/HomeManagement.App.Android/MainActivity.cs
+++ HomeManagement.App/HomeManagement.App.Android/MainActivity.cs
@@ -1,15 +1,12 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace HomeManagement.App.Droid
 {
-    [Activity(Label = "HomeManagement.App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HomeManagement.App", Icon = "@drawable/HomeManagement_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -18,7 +15,6 @@ namespace HomeManagement.App.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
---
+++ HomeManagement.App/HomeManagement.App.Android/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace HomeManagement.App.Droid
+{
+    [Activity(Label = "Home Mgmt", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreen : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            // Create your application here
+        }
+    }
+}
