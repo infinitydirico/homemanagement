4f8d8ba68e3951f68fd314af6ef748b6c5a15b6a
Ramiro Di Rico <ramiro.dirico@projekt202.com>
2019-08-19 21:32:49 -0300

    Added new icons for swipe buttons on listview.

--- HomeManagement.App/HomeManagement.App/Views/AccountPages/AccountPage.xaml.cs
+++ HomeManagement.App/HomeManagement.App/Views/AccountPages/AccountPage.xaml.cs
@@ -2,7 +2,6 @@
 using HomeManagement.App.Data.Entities;
 using HomeManagement.App.ViewModels;
 using HomeManagement.App.Views.Charges;
-using System.Collections.Generic;
 using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -35,16 +34,36 @@ namespace HomeManagement.App.Views.AccountPages
             Navigation.PushAsync(new AddAccountPage());
         }
 
-        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        private async void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
         {
             var stackLayout = sender as StackLayout;
-            var actions = stackLayout.Children.Where(x => x.GetType().Equals(typeof(Image)));
+            var label = stackLayout.Children.First(x => x.GetType().Equals(typeof(Label))) as Label;
+            var actions = stackLayout.Children.Where(x => x.GetType().Equals(typeof(Button)));
 
             var trashBin = actions.First();
-            trashBin.IsVisible = e.Direction.Equals(SwipeDirection.Right);
-
             var edit = actions.Last();
-            edit.IsVisible = e.Direction.Equals(SwipeDirection.Right);
+
+            var offsetIn = e.Direction.Equals(SwipeDirection.Right) ? 35 : -35;
+            var offsetOut = e.Direction.Equals(SwipeDirection.Right) ? 70 : -70;
+
+            var rectIn = label.Bounds.Offset(offsetIn, 0);
+            var rectOut = label.Bounds.Offset(offsetOut, 0);
+
+            if ((label.Bounds.X < 50 && e.Direction.Equals(SwipeDirection.Right))
+                || (label.Bounds.X > 50 && e.Direction.Equals(SwipeDirection.Left)))
+            {
+                var animationIn = await label.LayoutTo(rectIn, easing: Easing.SinIn);
+
+                if (e.Direction.Equals(SwipeDirection.Left))
+                {
+                    trashBin.IsVisible = edit.IsVisible = false;
+                    rectOut.X = 0;
+                }
+
+                var animationOut = await label.LayoutTo(rectOut, easing: Easing.SinOut);
+            }
+            
+            trashBin.IsVisible = edit.IsVisible = e.Direction.Equals(SwipeDirection.Right);
         }
 
         private void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
@@ -53,7 +72,7 @@ namespace HomeManagement.App.Views.AccountPages
             var label = stackLayout.Children.First(x => x.GetType().Equals(typeof(Label))) as Label;
             var accounts = ((AccountsViewModel)accountsList.BindingContext).Accounts;
             var account = accounts.First(x => x.Name.Equals(label.Text));
-            Navigation.PushAsync(new ChargesList(account));
+            Navigation.PushAsync(new ChargesList(account));            
         }
     }
 }
