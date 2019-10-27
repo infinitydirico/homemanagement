857b9b63aabe90f5be41c3d93be2e8c637542c44
Ramiro Di Rico <ramiro.dirico@projekt202.com>
2019-10-26 21:09:40 -0300

    added page to add configuration settings

--- HomeManagement.AdminSite/Controllers/ConfigurationSettingsController.cs
+++ HomeManagement.AdminSite/Controllers/ConfigurationSettingsController.cs
@@ -2,28 +2,32 @@
 using HomeManagement.AdminSite.Services;
 using HomeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HomeManagement.AdminSite.Controllers
 {
+    [Authorize]
     public class ConfigurationSettingsController : Controller
     {
         private readonly IConfigurationSettingsService configurationSettingsService;
+        private readonly IMemoryCache _cache;
 
-        public ConfigurationSettingsController(IConfigurationSettingsService configurationSettingsService)
+        public ConfigurationSettingsController(IConfigurationSettingsService configurationSettingsService,
+            IMemoryCache cache)
         {
             this.configurationSettingsService = configurationSettingsService;
+            _cache = cache;
         }
 
-        [Authorize]
         public async Task<IActionResult> Index()
         {
-            var configs = await configurationSettingsService.GetSettings();
+            var configs = await GetSettings();
             return View(configs);
         }
-
-        [Authorize]
+        
         [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
@@ -32,7 +36,7 @@ namespace HomeManagement.AdminSite.Controllers
                 return RedirectToAction("Error", "Home");
             }
 
-            var configs = await configurationSettingsService.GetSettings();
+            var configs = await GetSettings();
             var config = configs.First(x => x.Id.Equals(id));
             return View(config);
         }
@@ -48,7 +52,50 @@ namespace HomeManagement.AdminSite.Controllers
             {
                 return RedirectToAction("Error", "Home");
             }
+            FlushConsigurationSettingsCache();
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConfigurationSettingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await configurationSettingsService.Update(model);
+            }
+            else
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            FlushConsigurationSettingsCache();
+            return RedirectToAction("Index");
+        }
+
+        private async Task<IEnumerable<ConfigurationSettingModel>> GetSettings()
+        {
+            IEnumerable<ConfigurationSettingModel> configs = _cache.Get(nameof(ConfigurationSettingModel)) as IEnumerable<ConfigurationSettingModel>;
+            if (configs != null)
+            {
+                return configs;
+            }
+
+            configs = await configurationSettingsService.GetSettings();
+
+            _cache.CreateEntry(nameof(ConfigurationSettingModel));
+            _cache.Set(nameof(ConfigurationSettingModel), configs);
+
+            return configs;
+        }
+
+        private void FlushConsigurationSettingsCache()
+        {
+            _cache.Remove(nameof(ConfigurationSettingModel));
         }
     }
 }
